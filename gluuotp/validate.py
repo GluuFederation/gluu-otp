67b3c65a1c145093883e5bad0ff443ee1df884e4
Arunmozhi <arun@arunmozhi.in>
2016-06-23 07:50:53 +0530

    reduced the library to a single validate module

--- gluuotp/validate.py
+++ gluuotp/validate.py
@@ -1,106 +1,69 @@
 import re
-import json
-
-import status
 
 from Crypto.Cipher import AES
 
-from ldapdriver import LDAPConnection
+OK = 'OK'
+BAD_OTP = 'BAD_OTP'
+REPLAYED_OTP = 'REPLAYED_OTP'
+
+# sorry for this one-liner
+modhex = ''.join(dict([('cbdefghijklnrtuv'[i], '0123456789abcdef'[i])
+                 for i in range(16)]).get(chr(j), '?') for j in range(256))
 
 
-class Validate(object):
+def modhexdecode(string):
+    return string.translate(modhex).decode('hex')
 
-    # sorry for this one-liner
-    modhex = ''.join(dict([('cbdefghijklnrtuv'[i], '0123456789abcdef'[i])
-                     for i in range(16)]).get(chr(j), '?') for j in range(256))
 
-    def __init__(self):
-        self.ldap = LDAPConnection()
+def CRC(data):
+    crc = 0xffff
+    for b in data:
+        crc ^= (ord(b) & 0xff)
+        for j in range(0, 8):
+            n = crc & 1
+            crc >>= 1
+            if n != 0:
+                crc ^= 0x8408
+    return crc
 
-    def modhexdecode(self, string):
-        return string.translate(self.modhex).decode('hex')
 
-    def CRC(self, data):
-        crc = 0xffff
-        for b in data:
-            crc ^= (ord(b) & 0xff)
-            for j in range(0, 8):
-                n = crc & 1
-                crc >>= 1
-                if n != 0:
-                    crc ^= 0x8408
-        return crc
+def validate(otp, internalname, aeskey, counter, time):
+    """Validates the OTP.
 
-    def validate_user(self, username, otp):
-        """Validates the OTP of the requested user.
+    Params:
+        otp (string) - The otp string
+        internalname (string) - the internal name of the Yubikey
+        aeskey (string) - the aes key of the Yubikey
+        counter (int) - the value of counter stored in DB
+        time (int) - the timestamp of last used otp stored in DB
 
-        This function is written specifically to address the requirement of
-        username in order to fetch the gluuOTPMetadata fromt he LDAP server.
-        This has no particular use when using a RDBMS like SQLITE.
+    Returns:
+        status (string) - status of the evaluation
+   """
+    match = re.match('([cbdefghijklnrtuv]{0,16})([cbdefghijklnrtuv]{32})',
+                     otp)
+    if not match:
+        return BAD_OTP
 
-        The format for gluuOTPMetadata is a <string> of the following JSON
-        {
-            'publicname': 'string',
-            'internalname': 'string',
-            'aeskey': 'string',
-            'counter': int,
-            'time': int
-        }
+    public_name, token = match.groups()
 
-        Params:
-            username (string) - the username stored in the ldap
-            otp (string) - the otp by the yubikey
-        """
-        match = re.match('([cbdefghijklnrtuv]{0,16})([cbdefghijklnrtuv]{32})',
-                         otp)
-        if not match:
-            return status.BAD_OTP
+    aes = AES.new(aeskey.decode('hex'), AES.MODE_ECB)
+    plaintext = aes.decrypt(modhexdecode(token)).encode('hex')
 
-        public_name, token = match.groups()
+    if internalname != plaintext[:12]:
+        return BAD_OTP
 
-        keys = self.ldap.get_keys(username)
-        # the user doesn't have any Yubikeys registered against his name
-        if not keys:
-            return status.UNREGISTERED_KEY
+    if CRC(plaintext[:32].decode('hex')) != 0xf0b8:
+        return BAD_OTP
 
-        key_data = None
-        for key in keys:
-            if public_name in key:
-                key_data = key
+    internalcounter = int(plaintext[14:16] + plaintext[12:14] +
+                          plaintext[22:24], 16)
+    if counter >= internalcounter:
+        return REPLAYED_OTP
 
-        # None of the user's keys match the OTP key
-        if not key_data:
-            return status.UNREGISTERED_KEY
+    timestamp = int(plaintext[20:22] + plaintext[18:20] + plaintext[16:18],
+                    16)
+    if time >= timestamp and (counter >> 8) == (internalcounter >> 8):
+        return BAD_OTP
 
-        key = json.loads(key_data)
-        aeskey = key['aeskey']
-        internalname = key['internalname']
-        counter = key['counter']
-        time = key['time']
-
-        aes = AES.new(aeskey.decode('hex'), AES.MODE_ECB)
-        plaintext = aes.decrypt(self.modhexdecode(token)).encode('hex')
-
-        if internalname != plaintext[:12]:
-            return status.BAD_OTP
-
-        if self.CRC(plaintext[:32].decode('hex')) != 0xf0b8:
-            return status.BAD_OTP
-
-        internalcounter = int(plaintext[14:16] + plaintext[12:14] +
-                              plaintext[22:24], 16)
-        if counter >= internalcounter:
-            return status.REPLAYED_OTP
-
-        timestamp = int(plaintext[20:22] + plaintext[18:20] + plaintext[16:18],
-                        16)
-        if time >= timestamp and (counter >> 8) == (internalcounter >> 8):
-            return status.BAD_OTP
-
-        # IMPORTANT: Updating with the counter and time is important to avoid
-        #            reuse of OTPs
-        key['counter'] = internalcounter
-        key['time'] = timestamp
-        self.ldap.update_key(username, key)
-
-        return status.OK
+    return OK
